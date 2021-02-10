06baa0dd22ab6c7f974b9cb6cc35ea96f1ad6adb
mattyg915 <matt.gercz@icloud.com>
2021-02-09 20:23:07 -0600

    GET and POST both up and running

--- service/server.py
+++ service/server.py
@@ -1,12 +1,13 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import ThreadingMixIn
 from urllib.parse import urlparse
+import json
 import sys
 import os
 
 import sqlite3
-import threading
 
+rec = False
 path = os.path.dirname(os.path.abspath(__file__))
 dbPath = os.path.join(path, 'kv.db')
 
@@ -16,32 +17,51 @@ class RequestHandler(BaseHTTPRequestHandler):
     cursor = connection.cursor()
 
     def do_GET(self):
-        print(threading.currentThread().getName())
-        parsed_path = urlparse(self.path)
+        route = urlparse(self.path)
         # handle read request
-        if parsed_path.path == "/kv739/":
-            print("request received")
-            k = parsed_path.query.split("=")[-1]
-            print(k)
+        if route.path == "/kv739/":
+            k = route.query.split("=")[-1]
+            query = (k,)
+            cursor.execute('''SELECT value FROM 'records' WHERE key=?''', query)
+            result = cursor.fetchone()
+
+            s = {"is_key_in": "yes", "value": result[0]} if result != None else {"is_key_in": "no", "value": "None"}
+            response = json.dumps(s)
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.send_header("Content-Length", len(response))
+            self.end_headers()
+            self.wfile.write(response.encode())
             return
 
+    def do_POST(self):
+        content_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
 
-class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+        decoded_body = post_body.decode()
+        body = json.loads(decoded_body)
+        route = urlparse(self.path)
+        # direct write request
+        if route.path == "/kv739/":
+
+            key, value = body['key'], body['value']
+
+            cursor.execute('''INSERT INTO 'records' VALUES (?, ?)''', (key, value))
+            connection.commit()
+
+class Server(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
 
 if __name__ == '__main__':
     ip, port = sys.argv[1], sys.argv[2]
-    # reconnect to the database
-    # key->value
-
     connection = sqlite3.connect(dbPath)
     cursor = connection.cursor()
 
     # make sure we only create the table once
-    cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='values' ''')
-    if cursor.fetchone()[0] == 1:
-        cursor.execute('''CREATE TABLE values (key text, value text)''')
+    cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='records' ''')
+    if cursor.fetchone()[0] == 0:
+        cursor.execute('''CREATE TABLE 'records' (key text, value text)''')
 
-    server = ThreadedHTTPServer((ip, int(port)), RequestHandler)
+    server = HTTPServer((ip, int(port)), RequestHandler)
     print('Server initalizing, reachable at http://{}:{}'.format(ip, port))
     server.serve_forever()

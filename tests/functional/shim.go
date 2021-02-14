01b4dd751af186ff598641f3e1c2c74f531f69c9
Deepak Sirone <deepaksirone94@gmail.com>
2021-02-13 21:38:51 -0600

    Update shim

--- tests/functional/shim.go
+++ tests/functional/shim.go
@@ -6,7 +6,7 @@ package main
 #include <stdlib.h>
 */
 import "C"
-import "unsafe"
+//import "unsafe"
 
 func Kv739_init(address string) int32 {
 	return int32(C.kv739_init(C.CString(address)))
@@ -16,7 +16,32 @@ func Kv739_get(key string, value []byte) int32 {
 	p := C.malloc(C.size_t(len(value)))
 	defer C.free(p)
 
-	return 0
-	//ret := int32(C.kv739_get(C.CString(key), unsafe.Pointer(p)))
-	
+	ret := int32(C.kv739_get(C.CString(key), (*C.char)(p)))
+	if ret == 0 {
+                cBuf := (*[2049]byte)(p)
+                copy(value[:], cBuf[:])
+        } else {
+                value[0] = 0x0
+        }
+
+        return ret
+}
+
+func Kv739_put(key string, value string, old_value []byte) int32 {
+        p := C.malloc(C.size_t(len(old_value)))
+        defer C.free(p)
+
+        ret := int32(C.kv739_put(C.CString(key), C.CString(value), (*C.char)(p)))
+	if ret == 0 {
+		cBuf := (*[2049]byte)(p)
+                copy(old_value[:], cBuf[:])
+	} else {
+		old_value[0] = 0x0
+	}
+
+	return ret
+}
+
+func Kv739_shutdown() int32 {
+	return int32(C.kv739_shutdown())
 }

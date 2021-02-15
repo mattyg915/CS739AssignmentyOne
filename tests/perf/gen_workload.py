7ccac799adecb8fa3899e64f1049449243dd26a1
MandrakeCQC <kchen359@wisc.edu>
2021-02-15 00:42:47 -0600

    db populate done, puts problematic

---
+++ tests/perf/gen_workload.py
@@ -0,0 +1,53 @@
+import random 
+import bisect 
+import math
+
+import argparse
+
+latest_percentile = 90
+
+class ZipfGenerator: 
+
+    def __init__(self, n, alpha): 
+        # Calculate Zeta values from 1 to n: 
+        tmp = [1. / (math.pow(float(i), alpha)) for i in range(1, n+1)] 
+        zeta = reduce(lambda sums, x: sums + [sums[-1] + x], tmp, [0]) 
+
+        # Store the translation map: 
+        self.distMap = [x / zeta[-1] for x in zeta] 
+
+    def next(self): 
+        # Take a uniform 0-1 pseudo-random value: 
+        u = random.random()  
+
+        # Translate the Zipf variable: 
+        return bisect.bisect(self.distMap, u) - 1
+
+
+def main():
+    print("hi")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--distribution', dest='distribution', help="distribution from: zipf, uniform, sequential, latest", required=True)
+    parser.add_argument('-r', '--read', dest='read_percent', type=int, help="read percentile from 0 to 100. Write percentile will be 100-read percentile", required=True)
+    parser.add_argument('-a', '--alpha', dest='alpha', type=int, default=100, help="alpha value for zipf distribution")
+    parser.add_argument('-o', '--out', dest='out', type=argparse.FileType('w', encoding='ascii'), help="output file name", required=True)
+    parser.add_argument('-n', '--number', dest='n', type=int, help="number of requests in 100s, i.e. n=10 means 1k requests", required=True)
+    args = parser.parse_args()
+    print(args)
+    
+    if (args.distribution='zipf'):
+        for i in range(0,args.n):
+            args.out.write('abcd')
+    elif (args.distribution='uniform'):
+    elif (args.distribution='sequential'):
+    elif (args.distribution='latest'):
+    else:
+        print('invalid distribution')
+    
+    
+    args.out.write('abcd')
+    args.out.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/perf/perftest.c
@@ -0,0 +1,87 @@
+#include "lib739kv.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+const char delim[2] = "[";
+const size_t keylenMax=128;
+const size_t vallenMax=2048;
+
+static void split_kv(char * line, char* key, char* val) {
+    char* token = strtok(line, delim);
+    //token = strtok(NULL, delim, &state2);
+    while( token != NULL ) {
+      if (key == NULL) {
+          key = strdup(token);
+          printf( " key: %s\n", key );
+          token = strtok(NULL, delim);
+      }
+      else {
+          val = strdup(token);
+          printf( " value: %s\n", val );
+          token = strtok(NULL, delim);
+      }
+    }
+    
+    /*char *state1, *state2;
+    char *token = strtok_r(line, delim, &state1);
+    while(token){
+        char *current_string = strdup(token);
+
+        char *tk = strtok_r(current_string, delim, &state2); // KEY
+        printf("key: %s \r\n", tk);
+        tk = strtok_r(NULL, delim, &state2);                    // VALUE
+        printf("value: %s\r\n", tk);
+        printf("%s\n", token);
+        free(current_string);
+        break;
+    }*/
+}
+
+int main( int argc, char *argv[] )
+{
+    //usage: ./prog dbfile    
+    //tracefile
+    
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    
+    FILE *dbfp = fopen(argv[1], "r");
+    
+    
+	kv739_init("127.0.0.1:5000");
+	char value[2049];
+	printf("Putting Value now!\n");
+
+	
+    
+    while ((read = getline(&line, &len, dbfp)) != -1) {
+        printf("Retrieved line of length %zu:\n", read);
+        printf("line: %s\n", line);
+        
+        //char * key [keylenMax+1];
+        //char * val [vallenMax+1];
+        char* key = NULL;
+        char* val = NULL;
+        split_kv(line, key, val);
+        
+        if (kv739_put(key, val, value) >= 0)
+		printf("Value inserted\n");
+        if (kv739_get(key, value) == 0)
+            printf("%s\n", value);
+        
+        free(key);
+        free(val);
+        //printf("key: %s\n", key);
+        //printf("val: %s\n\n", val);
+    }
+
+    kv739_shutdown();
+
+    fclose(dbfp);
+
+    //FILE *tracefp = fopen(argv[1], "r");
+    
+	return 0;
+}

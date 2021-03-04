8eef929926d75bc45b41947908fe9dec24ade7d2
mattyg915 <matt.gercz@icloud.com>
2021-03-03 21:43:07 -0600

    indent fixes

--- service/server.py
+++ service/server.py
@@ -15,18 +15,11 @@ import sqlite3
 
 path = os.path.dirname(os.path.abspath(__file__))
 
-# caching = False
-# cache_size = 0
-# cache_size =
-# cache = OrderedDict()
-
 KEEP_RUNNING = True
 ENTROPY_MAX = 1
 conn_index = 0
 
 class HandleRequests(BaseHTTPRequestHandler):
-
-
     # disable logging
     def log_message(self, format, *args):
         return
@@ -127,60 +120,6 @@ class HandleRequests(BaseHTTPRequestHandler):
                 if success:
                     print("successfully removed a server from reachable: host = %s, port = %d" % (host, port))
 
-    # all requests handled via POST
-    # def do_GET(self):
-    #     connection = sqlite3.connect(dbPath)
-    #     cursor = connection.cursor()
-    #     route = urlparse(self.path)
-    #     # handle read request
-    #     if route.path == "/kv739/":
-    #         key = route.query.split("=")[-1]
-    #         query = (key,)
-    #         cache_hit = False
-    #
-    #         # if caching enabled check the cache
-    #         if caching:
-    #             if key in cache.keys():
-    #                 result = [cache[key]]
-    #                 cache_hit = True
-    #             else:
-    #                 cursor.execute('''SELECT value FROM 'records' WHERE key=?''', query)
-    #                 result = cursor.fetchone()
-    #                 cache_hit = False
-    #
-    #         else:
-    #             cursor.execute('''SELECT value FROM 'records' WHERE key=?''', query)
-    #             result = cursor.fetchone()
-    #
-    #         if result is not None:
-    #             package = {"exists": "yes", "value": result[0]}
-    #
-    #             if caching and not cache_hit:
-    #                 # cache is FIFO
-    #                 if len(cache.keys()) > 250:
-    #                     cache.popitem(last=False)
-    #                     cache[key] = result
-    #                 else:
-    #                     cache[key] = result
-    #         else:
-    #             package = {"exists": "no", "value": "None"}
-    #
-    #         response = json.dumps(package)
-    #
-    #         self.send_response(200)
-    #         self.send_header('Content-type', 'application/json')
-    #         self.send_header("Content-Length", str(len(response)))
-    #         self.end_headers()
-    #         self.wfile.write(response.encode())
-    #         return
-    #     else:
-    #         self.send_response(200)
-    #         #self.send_header('Content-type', 'application/json')
-    #         self.send_header("Content-Length", str(len("helloworld")))
-    #         self.end_headers()
-    #         self.wfile.write("helloworld".encode())
-    #         return
-
     def do_POST(self):
         try:
             connection = sqlite3.connect(dbPath, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
@@ -243,9 +182,6 @@ class HandleRequests(BaseHTTPRequestHandler):
                 self.wfile.write(response.encode())
                 return
 
-
-            # cache_hit = False
-
             # validate strings
             valid_string = self.validate_string(key)
             if (valid_string is not True):
@@ -253,27 +189,6 @@ class HandleRequests(BaseHTTPRequestHandler):
 
             query = (key,)
 
-            # if caching:
-            #     if key in cache.keys():
-            #         result = [cache[key]]
-            #         cache_hit = True
-            #     else:
-            #         try:
-            #             cursor.execute('''SELECT value FROM 'records' WHERE key=?''', query)
-            #             result = cursor.fetchone()
-            #             cache_hit = False
-            #         except Exception:
-            #             package = {"error": "Internal server error"}
-            #             response = json.dumps(package)
-            #
-            #             self.send_response(500)
-            #             self.send_header('Content-type', 'application/json')
-            #             self.send_header("Content-Length", str(len(response)))
-            #             self.end_headers()
-            #             self.wfile.write(response.encode())
-            #             return
-
-            # else:
             try:
                 cursor.execute('''SELECT value FROM 'records' WHERE key=?''', query)
                 result = cursor.fetchone()
@@ -294,13 +209,6 @@ class HandleRequests(BaseHTTPRequestHandler):
             if method == 'get':
                 if result is not None:
                     package = {"exists": "yes", "former_value": result[0], "new_value": "[]"}
-                    # if caching and not cache_hit:
-                    #     # cache is FIFO
-                    #     if len(cache.keys()) > cache_size:
-                    #         cache.popitem(last=False)
-                    #         cache[key] = result
-                    #     else:
-                    #         cache[key] = result
 
                 else:
                     package = {"exists": "no", "former_value": "[]", "new_value": "[]"}
@@ -380,8 +288,8 @@ class HandleRequests(BaseHTTPRequestHandler):
                 query = (key,)
                 
                 try:
-                cursor.execute('''SELECT value FROM 'records' WHERE key=?''', query)
-                result = cursor.fetchone()
+                    cursor.execute('''SELECT value FROM 'records' WHERE key=?''', query)
+                    result = cursor.fetchone()
                 except Exception as e:
                     message = "Internal server error: {}".format(e)
                     package = {"error": message}
@@ -400,17 +308,17 @@ class HandleRequests(BaseHTTPRequestHandler):
                         try:
                                 cursor.execute('''UPDATE 'records' SET value = ?, time = ? WHERE key = ?''', (value, millisec, key))
                                 connection.commit()
-                            except Exception as e:
-                                message = "Internal server error: {}".format(e)
-                                package = {"error": message}
-                                response = json.dumps(package)
+                        except Exception as e:
+                            message = "Internal server error: {}".format(e)
+                            package = {"error": message}
+                            response = json.dumps(package)
 
-                                self.send_response(500)
-                                self.send_header('Content-type', 'application/json')
-                                self.send_header("Content-Length", str(len(response)))
-                                self.end_headers()
-                                self.wfile.write(response.encode())
-                                return
+                            self.send_response(500)
+                            self.send_header('Content-type', 'application/json')
+                            self.send_header("Content-Length", str(len(response)))
+                            self.end_headers()
+                            self.wfile.write(response.encode())
+                            return
                     else:
                         try:
                             cursor.execute('''INSERT INTO 'records' VALUES (?, ?, ?)''', (key, value, millisec))
@@ -490,8 +398,6 @@ class Server(ThreadingMixIn, HTTPServer):
             parts = node_list[i].split()
             conns.append(http.client.HTTPConnection(parts[0], port=parts[1]))
             print('connected to a peer')
-
-
 
         dbName = port + 'kv.db'
         dbPath = os.path.join(path, dbName)
